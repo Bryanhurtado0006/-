ebecf3efdc7a09f60d726c0677e818a50400a8fc
Victor Alejandro@Alejandro_lope <Victor Alejandro@Alejandro_lopez>
2025-04-10 15:32:59 -0500

    bro manito material commit.

--- app/src/main/java/com/example/myapplicationics/MainActivity3victor.java
+++ app/src/main/java/com/example/myapplicationics/MainActivity3victor.java
@@ -1,6 +1,7 @@
 package com.example.myapplicationics;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3victor extends AppCompatActivity {
 
+Button btn_sociales,btn_naturales,btn_lectura,btn_matematicas,btn_ingles;
+
+
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 
 	setContentView(R.layout.activity_main_activity3victor);
+	
+	btn_ingles=findViewById(R.id.btn_ingles);
+	btn_lectura=findViewById(R.id.btn_Lectura_critica);
+	btn_matematicas=findViewById(R.id.btn_Matematicas);
+	btn_sociales=findViewById(R.id.btn_ciencias_sociales);
+	btn_naturales=findViewById(R.id.btn_ciencias_naturales);
+	
+	
 	
 }
 }
