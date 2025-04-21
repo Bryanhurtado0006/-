26e4b60be9fe0ed41de27aefd7f52e6b38430a91
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-04-21 17:45:28 -0500

    se implemento viewBinding en build

--- app/src/main/java/com/example/myapplicationics/MainActivitybryan3.java
+++ app/src/main/java/com/example/myapplicationics/MainActivitybryan3.java
@@ -1,20 +1,21 @@
 package com.example.myapplicationics;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.myapplicationics.databinding.ActivityMainActivitybryan3Binding;
 
 public class MainActivitybryan3 extends AppCompatActivity {
+    private ActivityMainActivitybryan3Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main_activitybryan3);
+        binding = ActivityMainActivitybryan3Binding.inflate(getLayoutInflater());
+
+
+        setContentView(binding.getRoot());
+
 
     }
 }
