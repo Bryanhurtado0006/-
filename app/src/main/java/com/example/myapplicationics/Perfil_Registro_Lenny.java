26aafbcd09dc760d60d4f6991758b9df6129661f
ADSO Tarde <lennyjazz98@gmail.com>
2025-04-21 17:42:13 -0500

    lenny

---
+++ app/src/main/java/com/example/myapplicationics/Perfil_Registro_Lenny.java
@@ -0,0 +1,19 @@
+package com.example.myapplicationics;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Perfil_Registro_Lenny extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_registro_lenny);
+
+    }
+}
