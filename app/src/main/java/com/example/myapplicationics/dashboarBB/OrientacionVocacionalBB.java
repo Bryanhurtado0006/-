03b227a19ac63f5a8b433a9518352d97c3d0fafd
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-05-05 18:52:20 -0500

    bryan

---
+++ app/src/main/java/com/example/myapplicationics/dashboarBB/OrientacionVocacionalBB.java
@@ -0,0 +1,26 @@
+package com.example.myapplicationics.dashboarBB;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplicationics.R;
+
+public class OrientacionVocacionalBB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_orientacion_vocacional_bb);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
