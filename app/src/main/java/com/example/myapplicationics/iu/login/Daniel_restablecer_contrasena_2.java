7a41c3017c212e6f36861825f0f531d7efb82340
Maria <mp3864926@gmail.com>
2025-05-05 15:08:40 -0500

    Cambios Daniel

--- app/src/main/java/com/example/myapplicationics/iu/login/Daniel_restablecer_contrasena_2.java
+++ app/src/main/java/com/example/myapplicationics/iu/login/Daniel_restablecer_contrasena_2.java
@@ -9,18 +9,15 @@ import com.example.myapplicationics.databinding.ActivityDanielRestablecerContras
 
 public class Daniel_restablecer_contrasena_2 extends AppCompatActivity {
 
-    // ViewBinding
     private ActivityDanielRestablecerContrasena2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Inicializar ViewBinding
         binding = ActivityDanielRestablecerContrasena2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Botón que navega al siguiente activity
         binding.btnContinuar.setOnClickListener(v -> {
             Intent intent = new Intent(this, Daniel_restablecer_contrasena_3.class);
             startActivity(intent);
