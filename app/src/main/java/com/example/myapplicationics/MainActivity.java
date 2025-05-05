b45aec9bde7069ca87c28137a062780d03bdbd96
Maria2007 <anamariazuniga202@gmail.com>
2025-05-05 14:08:29 -0500

    cambio mayo 5

--- app/src/main/java/com/example/myapplicationics/MainActivity.java
+++ app/src/main/java/com/example/myapplicationics/MainActivity.java
@@ -11,40 +11,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplicationics.ui.simulacroANA.Inicio_Simulacros;
+
 public class MainActivity extends AppCompatActivity {
-Button btn_Material;
-Button btn_Registro;
-  @Override
+    Button btn_Material;
+    Button btn_Registro;
+
+    Button btnSimulacro_Estadistica;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_main);
-        
-        btn_Material=findViewById(R.id.btn_Material);
-        
-        
+
+        btn_Material = findViewById(R.id.btn_Material);
+
+
         btn_Material.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent irmaterial=new Intent(MainActivity.this,MainActivity3victor.class);
+                Intent irmaterial = new Intent(MainActivity.this, MainActivity3victor.class);
                 startActivity(irmaterial);
-                
+
             }
         });
 
 
-        btn_Registro=findViewById(R.id.btn_Registro);
+        btn_Registro = findViewById(R.id.btn_Registro);
 
 
         btn_Registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent irRegistro=new Intent(MainActivity.this,RegistroLenny.class);
+                Intent irRegistro = new Intent(MainActivity.this, RegistroLenny.class);
                 startActivity(irRegistro);
 
             }
         });
 
-        
     }
 }
+
--- app/src/main/java/com/example/myapplicationics/ui/simulacroANA/Inicio_Simulacros.java
+++ app/src/main/java/com/example/myapplicationics/ui/simulacroANA/Inicio_Simulacros.java
@@ -2,48 +2,43 @@ package com.example.myapplicationics.ui.simulacroANA;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.myapplicationics.databinding.ActivityInicioSimulacrosBinding;
 
 public class Inicio_Simulacros extends AppCompatActivity {
 
-    private ActivityInicioSimulacrosBinding binding; // Este es el binding
+    private ActivityInicioSimulacrosBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
 
         binding = ActivityInicioSimulacrosBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-        binding.lectura.setOnClickListener(lectura -> {
-            Intent intent = new Intent(Inicio_Simulacros.this, Simulacro_Lectura_1.class);
+        binding.lectura.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Simulacro_Lectura_1.class);
             startActivity(intent);
+        });
 
-            binding.matematicas.setOnClickListener(matematicas -> {
-                Intent intent2 = new Intent(Inicio_Simulacros.this, Simulacro_Matematicas_1.class);
-                startActivity(intent2);
+        binding.matematicas.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Simulacro_Matematicas_1.class);
+            startActivity(intent);
+        });
 
-                binding.naturales.setOnClickListener(naturales -> {
-                    Intent intent3 = new Intent(Inicio_Simulacros.this, Simulacro_Naturales_1.class);
-                    startActivity(intent3);
+        binding.naturales.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Simulacro_Naturales_1.class);
+            startActivity(intent);
+        });
 
-                    binding.sociales.setOnClickListener(sociales -> {
-                        Intent intent4 = new Intent(Inicio_Simulacros.this, Simulacro_Sociales_1.class);
-                        startActivity(intent4);
+        binding.sociales.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Simulacro_Sociales_1.class);
+            startActivity(intent);
+        });
 
-                        binding.ingles.setOnClickListener(ingles -> {
-                            Intent intent5 = new Intent(Inicio_Simulacros.this, Simulacro_Ingles_1.class);
-                            startActivity(intent5);
-                        });
-                    });
-                });
-            });
+        binding.ingles.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Simulacro_Ingles_1.class);
+            startActivity(intent);
         });
     }
 }
-
