ee7d32bd51e6f524af94f14acb8c0166828f1897
Maria <mp3864926@gmail.com>
2025-05-05 15:29:48 -0500

    Cambios Daniel

--- app/src/main/java/com/example/myapplicationics/iu/login/Inicio_Sesion_Principal.java
+++ app/src/main/java/com/example/myapplicationics/iu/login/Inicio_Sesion_Principal.java
@@ -1,6 +1,8 @@
 package com.example.myapplicationics.iu.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +19,17 @@ public class Inicio_Sesion_Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio_sesion_principal);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView tvRestablecer = findViewById(R.id.ed_restablecer_contraseña_Daniel);
+        tvRestablecer.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Daniel_restablecer_contrasena_1.class);
+            startActivity(intent);
         });
     }
 }
