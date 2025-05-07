721250e40b14459a92150952323d2a288f3bf21d
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-05-06 23:08:13 -0500

    mañana sigoo

---
+++ app/src/main/java/com/example/myapplicationics/dashboarBB/OrientacionV/OrientacionVocacionalBB.java
@@ -0,0 +1,18 @@
+package com.example.myapplicationics.dashboarBB.OrientacionV;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplicationics.R;
+
+public class OrientacionVocacionalBB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_orientacion_vocacional_bb);
+
+    }
+}
