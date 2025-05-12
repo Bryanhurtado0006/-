64255240b07012d4cc618f3ac51f62c87f97565b
lenny pechene <lennyjazz98@gmail.com>
2025-05-12 13:58:00 -0500

    cambios lenny2

---
+++ app/src/main/java/Lenny_Configuracion/Lenny_Editarperfil.java
@@ -0,0 +1,20 @@
+package Lenny_Configuracion;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplicationics.R;
+
+public class Lenny_Editarperfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.activity_lenny_editarperfil2);
+    }
+}
---
+++ app/src/main/java/Lenny_Configuracion/Lenny_Reporte.java
@@ -0,0 +1,21 @@
+package Lenny_Configuracion;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplicationics.R;
+
+public class Lenny_Reporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.activity_lenny_reporte);
+
+    }
+}
