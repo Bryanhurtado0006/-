d11e81cd791473a32a67f3f8e39d20f33f4eafcf
alejo <lluisalejandro2007@gmail.com>
2025-05-19 14:15:09 -0500

    aa

--- app/src/main/java/com/example/myapplicationics/Gamificacion_LUIS/Luis_Gamificacion_interface.java
+++ app/src/main/java/com/example/myapplicationics/Gamificacion_LUIS/Luis_Gamificacion_interface.java
@@ -1,8 +1,6 @@
-package com.example.myapplicationics.iu.login;
+package com.example.myapplicationics.Gamificacion_LUIS;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,26 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplicationics.R;
 
-public class SplashActivity extends AppCompatActivity {
-
-    private static final int SPLASH_DURATION = 2000; // 3 segundos
+public class Luis_Gamificacion_interface extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-
+        setContentView(R.layout.activity_luis_gamificacion_interface);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }, SPLASH_DURATION);
     }
 }
